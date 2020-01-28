5793826f8b15334889aba234c35db30beecfe1df
Jelle Druyts <jelle_druyts@yahoo.com>
2020-01-28 12:48:41 +0100

    Simplified gadget result rendering

--- WebApp/Gadgets/GadgetResponse.cs
+++ WebApp/Gadgets/GadgetResponse.cs
@@ -1,14 +1,32 @@
 using System;
+using System.Text.Json.Serialization;
 
 namespace InspectorGadget.WebApp.Gadgets
 {
-    public class GadgetResponse<TResult>
+    public class GadgetResponse
     {
         public string MachineName { get; set; } = Environment.MachineName;
         public DateTimeOffset TimeStarted { get; set; } = DateTimeOffset.UtcNow;
         public DateTimeOffset TimeCompleted { get; set; }
         public string Error { get; set; }
-        public TResult Result { get; set; }
-        public GadgetResponse<TResult> ChainedResponse { get; set; }
+        [JsonIgnore]
+        public object ResultObject { get; set; }
+        [JsonIgnore]
+        public GadgetResponse ChainedResponseObject { get; set; }
+    }
+
+    public class GadgetResponse<TResult> : GadgetResponse
+    {
+        public TResult Result
+        {
+            get { return (TResult)this.ResultObject; }
+            set { this.ResultObject = value; }
+        }
+
+        public GadgetResponse<TResult> ChainedResponse
+        {
+            get { return (GadgetResponse<TResult>)this.ChainedResponseObject; }
+            set { this.ChainedResponseObject = value; }
+        }
     }
 }
