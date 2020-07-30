3f7e0d6b67d42ff9796074f654fd7e80cf114cac
Jelle Druyts <jelle_druyts@yahoo.com>
2020-07-30 22:12:33 +0200

    Removed HSTS to allow HTTP; fixed double HTML encoding

--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -38,8 +38,6 @@ namespace InspectorGadget.WebApp
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
             }
 
             app.UseStaticFiles();
